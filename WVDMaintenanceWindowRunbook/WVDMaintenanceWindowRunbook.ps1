45e77516f9dc4cfc9493be3071370c3a3958462e
robmc-itpro <54541411+robmc-itpro@users.noreply.github.com>
2020-07-16 14:15:14 +0100

    renamed runbook and added maintenance window runbook

---
+++ WVDMaintenanceWindowRunbook/WVDMaintenanceWindowRunbook.ps1
@@ -0,0 +1,51 @@
+$connectionName = "AzureRunAsConnection"
+try
+{
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+
+    "Logging in to Azure..."
+    Add-AzureRmAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+}
+catch {
+    if (!$servicePrincipalConnection)
+    {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    } else{
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+}
+
+$AutomationResourceGroup = 'WVD-UK-Automation'
+$LogicAppName = 'WVD-UK-HP001_Autoscale_Scheduler'
+$AutomationAccountName = 'WVD-UK-AutomationAccount'
+$RunbookName = 'WVDAutoStartRunbook'
+$AzureContext = Get-AzSubscription -SubscriptionId $ServicePrincipalConnection.SubscriptionID
+$params = @{"AzureResourceGroup"="WVD-UK"}
+
+# Disable the Auto-Scaling Logic App
+Write-Out "Disabling WVD Auto-Scaling... "
+Set-AzureRMLogicApp -ResourceGroupName $AutomationResourceGroup -Name $LogicAppName -state Disabled
+
+# Start All WVD Hosts
+
+Start-AzAutomationRunbook `
+    –AutomationAccountName $AutomationAccountName `
+    –Name $RunBookName `
+    -ResourceGroupName $AutomationResourceGroup `
+    -AzContext $AzureContext `
+    –Parameters $params –Wait
+
+# Wait for 2 Hours
+Write-Out "Sleeping for 2 hours... "
+start-sleep 7200 -seconds
+
+# Re-enable the Auto-Scaling Logic App 
+Write-Out "Re-enabling WVD Auto-Scaling... "
+Set-AzureRMLogicApp -ResourceGroupName $AutomationResourceGroup -Name $LogicAppName -state Enabled
